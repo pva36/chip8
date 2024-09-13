2fc33c52faae58d46dee6a63152b65b4b71fe9a0
Pedro Valdebenito Gutiérrez <154631161+pva36@users.noreply.github.com>
2024-09-13 08:17:47 -0300

    modify and simplify getters and setters for chip8's components.

--- js/chip8.js
+++ js/chip8.js
@@ -1,34 +1,34 @@
-class Uint8bit {
-  constructor() {
-    this._value = new Uint8Array(1);
-  }
-  get value() {
-    return this._value[0];
-  }
-  set value(value) {
-    if (typeof value !== "number" || value < 0) {
-      throw new Error("Must be an 8-bit unsigned integer");
-    } else {
-      this._value = value[0];
-    }
-  }
-}
-
-class Uint16bit {
-  constructor() {
-    this._value = new Uint16Array(1);
-  }
-  get value() {
-    return this._value[0];
-  }
-  set value(value) {
-    if (typeof value !== "number" || value < 0) {
-      throw new Error("Must be an 16-bit unsigned integer");
-    } else {
-      this._value[0] = value;
-    }
-  }
-}
+// class Uint8bit {
+//   constructor() {
+//     this._value = new Uint8Array(1);
+//   }
+//   get value() {
+//     return this._value[0];
+//   }
+//   set value(value) {
+//     if (typeof value !== "number" || value < 0) {
+//       throw new Error("Must be an 8-bit unsigned integer");
+//     } else {
+//       this._value = value[0];
+//     }
+//   }
+// }
+//
+// class Uint16bit {
+//   constructor() {
+//     this._value = new Uint16Array(1);
+//   }
+//   get value() {
+//     return this._value[0];
+//   }
+//   set value(value) {
+//     if (typeof value !== "number" || value < 0) {
+//       throw new Error("Must be an 16-bit unsigned integer");
+//     } else {
+//       this._value[0] = value;
+//     }
+//   }
+// }
 
 class Chip8 {
   constructor() {
@@ -38,17 +38,45 @@ class Chip8 {
     this.V = new Uint8Array(0x10);
 
     // I register (16-bit)
-    this.I = new Uint16bit();
+    // This is managed with the I getter and setter.
+    this._I = new Uint16Array(1);
 
     // TODO: delay and sound timers (8-bit registers)
 
     // Program Counter (16-bit)
-    this.PC = new Uint16bit();
+    this._PC = new Uint16Array(1);
 
     // Stack Pointer (8-bit)
-    this.SP = new Uint8bit();
+    this._SP = new Uint8Array(1);
 
     // Stack (array of 16 16-bit values)
     this.S = new Uint16Array(0x10);
   }
+
+  // Getter and setter for I register.
+  get I() {
+    return this._I[0];
+  }
+  set I(value) {
+    this._I[0] = value;
+  }
+
+  // Getter and setter for Program Counter
+  get PC() {
+    return this._PC[0];
+  }
+  set PC(value) {
+    this._PC[0] = value;
+  }
+
+  // Getter and setter for Stack Pointer
+  get SP() {
+    return this._PC[0];
+  }
+  set PC(value) {
+    this._PC[0] = value;
+  }
 }
+
+/* test */
+let chip8 = new Chip8();
