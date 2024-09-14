7cd0975de26e50e0bf8f3102af5b4716e08e6250
Pedro Valdebenito Gutiérrez <154631161+pva36@users.noreply.github.com>
2024-09-14 10:59:06 -0300

    add renderer class

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 import { Chip8 } from "./chip8.js";
+import { Renderer } from "./renderer.js";
+
+const canvas = document.querySelector("canvas#display");
 
 const chip8 = new Chip8();
 const reader = new FileReader();
-// let programBytes;
-let file;
 const fileInput = document.querySelector("input#file");
+const renderer = new Renderer(10, canvas, 64, 32);
 
-// console.log(fileInput);
+let file;
 
+// Renderer Test
+// renderer.renderDisplay(testArray);
+renderer.test_renderDisplay();
+
+// the cleanScreen method needs to be called inside a function, otherwise
+// the this inside clearScreen() will refer to the global object.
+setTimeout(function () {
+  renderer.clearScreen();
+}, 5000);
+
+// Get rom from user and run emulator
 fileInput.addEventListener("change", async function (event) {
   file = event.target.files[0];
 
